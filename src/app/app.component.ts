364c0192e4c0a82334792ec6248c9732139b3cac
Nidia Ardila Piñeros <nidiaardila17@gmail.com>
2024-01-16 01:10:35 -0300

    crear servicio para guardar en el localStorage, crear plantilla

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { TasksService } from './services/tasks.service';
+
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+
   title = 'taskApp';
+
+  tasksList: string[] =[];
+  newTask: string = '';
+
+  //injectar el servicio
+  private _tasksService = inject(TasksService);
+
+  ngOnInit(): void {
+    this.tasksList = this._tasksService.getTasks();
+  }
+
+  addTask(){
+    this._tasksService.addTask(this.newTask);
+    this.newTask = '';
+    this.tasksList = this._tasksService.getTasks();
+  }
+
+  deleteTask(index:number){
+    this._tasksService.deleteTask(index);
+    this.tasksList = this._tasksService.getTasks();
+  }
 }
---
+++ src/app/services/tasks.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TasksService {
+
+  private localStorageKey = 'tasksList';
+
+  getTasks():string []{
+    return JSON.parse(localStorage.getItem(this.localStorageKey) as string) || [];
+  }
+
+  addTask(task: string){
+    const tasks = this.getTasks();
+    tasks.push(task);
+    localStorage.setItem(this.localStorageKey, JSON.stringify(tasks));
+  }
+
+  deleteTask(index: number){
+    const tasks = this.getTasks();
+    tasks.splice(index, 1);
+    localStorage.setItem(this.localStorageKey, JSON.stringify(tasks));
+  }
+}
